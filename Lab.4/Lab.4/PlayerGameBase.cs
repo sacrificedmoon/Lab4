aef63fff850bdfbebad50aa6747eb62da7dcbfd3
sacrificedmoon <55134243+sacrificedmoon@users.noreply.github.com>
2019-10-03 13:49:36 +0200

    Add files via upload

---
+++ Lab.4/Lab.4/PlayerGameBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._4
+{
+    class PlayerGameBase
+    {
+        public int PlayerSTARTHEALTH = 100;
+        public int playerCurrentHealth;
+        public bool playerIsAlive = true;
+
+        public PlayGameBase()
+        {
+
+        }
+    }
+}
---
+++ Lab.4/Lab.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            player.playerCurrentHealth = player.PlayerSTARTHEALTH;
+        }
+    }
+}
