cd1348336cd91810cc22f7aee6127ddfa3e51434
Robin Nurmisto <nurmisto.robin@gmail.com>
2019-10-15 10:44:06 +0200

    Made innerwalls not visible

--- Lab.4/Lab.4/DungeonMap.cs
+++ Lab.4/Lab.4/DungeonMap.cs
@@ -70,7 +70,11 @@ namespace Lab._4
                     }
                     else if (dungeonmap[y, x] == '#')
                     {
-                        roomObjectList.Add(new Wall(y, x, true));
+                        if (x <= 40 & y <= 0 | x <= 40 & y == 21 | x == 0 & y <= 21 | x == 40 & y <= 21)
+                        {
+                            roomObjectList.Add(new Wall(y, x, true));
+                        }
+                        roomObjectList.Add(new Wall(y, x, false));
                     }
                     else if (dungeonmap[y, x] == '.')
                     {
@@ -110,11 +114,6 @@ namespace Lab._4
                 {
                     Console.SetCursorPosition(tile.Ypos, tile.Xpos);
                     Console.Write(tile.Symbol);                    
-                }
-                else if(tile is Wall)
-                {
-                    Console.SetCursorPosition(tile.Ypos, tile.Xpos);
-                    Console.Write(tile.Symbol);
                 }
             }
         }
